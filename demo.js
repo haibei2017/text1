7f245c2df8a49298919c57b5326feea108b1c5f7
haibei2017 <lihaiqin543@163.com>
2017-04-20 21:10:32 +0800

    create repository

---
+++ demo.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+// alert(Base.getId('box').innerHTML);
+// alert(Base.getName('sex')[0].value);
+// alert(Base.getTagName('p')[0].innerHTML);
+alert(Math.floor(0.7));
+}
